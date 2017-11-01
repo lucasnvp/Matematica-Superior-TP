2838508a1b61b2a33ca4361b29c971e12da764d9
Lucas Visser <lucasnvp@gmail.com>
2017-10-31 22:10:38 -0300

    Redondeo
    Se agrego el redondeo a todas las operaciones

--- ParabolaMinimosCuadrados.m
+++ ParabolaMinimosCuadrados.m
@@ -2,6 +2,7 @@ clc
 clear all
 
 %inicio
+Redondeo = input('ingrese la cantidad de decimales a trabajar :');
 X = input('ingrese los valores de X entre [] y separados por ; :');
 Y = input('ingrese los valores de Y entre [] y separados por ; :');
 [filas,columnas] = size(X);
@@ -11,30 +12,30 @@ X2Y = filas;
 Error = filas;
 
 % Armo la columna de x al cuadrado
-X2 = X.^2;
+X2 = round(X.^2, Redondeo);
 % Armo la columna de x al cubo
-X3 = X.^3;
+X3 = round(X.^3, Redondeo);
 % Armo la columna de x a la cuarta
-X4 = X.^4;
+X4 = round(X.^4, Redondeo);
 % Armo la columna de XY 
 for i=1:filas
     Nro(i,1) = i;
-    XY(i,1) = X(i,1) * Y(i,1);
+    XY(i,1) = round(X(i,1) * Y(i,1), Redondeo);
 end
 % Armo la columna de X2Y 
 for i=1:filas
-    X2Y(i,1) = X2(i,1) * Y(i,1);
+    X2Y(i,1) = round(X2(i,1) * Y(i,1), Redondeo);
 end
 
 % Sumatorias de las columnas
-sumatoriaN = sum(Nro(:,1));
-sumatoriaX = sum(X(:,1));
-sumatoriaX2 = sum(X2(:,1));
-sumatoriaX3 = sum(X3(:,1));
-sumatoriaX4 = sum(X4(:,1));
-sumatoriaY = sum(Y(:,1));
-sumatoriaXY = sum(XY(:,1));
-sumatoriaX2Y = sum(X2Y(:,1));
+sumatoriaN = round(sum(Nro(:,1)), Redondeo);
+sumatoriaX = round(sum(X(:,1)), Redondeo);
+sumatoriaX2 = round(sum(X2(:,1)), Redondeo);
+sumatoriaX3 = round(sum(X3(:,1)), Redondeo);
+sumatoriaX4 = round(sum(X4(:,1)), Redondeo);
+sumatoriaY = round(sum(Y(:,1)), Redondeo);
+sumatoriaXY = round(sum(XY(:,1)), Redondeo);
+sumatoriaX2Y = round(sum(X2Y(:,1)), Redondeo);
 
 % Sistemas de Ecuaciones
 a = [sumatoriaX4, sumatoriaX3, sumatoriaX2;
@@ -45,13 +46,13 @@ resultado = a\b;
 disp(resultado);
 
 % Funcion P(x) = ax^2+bx+c
-recorrido=0:20;
+recorrido=0:10;
 P = (resultado(1,1) * (recorrido.^2)) + resultado(2,1)*recorrido + resultado(3,1);
 
 % Error
 for i=1:filas
-    Px = resultado(1,1)*(X(i,1)^2) + resultado(2,1)*X(i,1) + resultado(3,1);
-    Error(i,1) = (Px - Y(i,1))^2;
+    Px = round(resultado(1,1)*(X(i,1)^2) + resultado(2,1)*X(i,1) + resultado(3,1), Redondeo);
+    Error(i,1) = round((Px - Y(i,1))^2, Redondeo);
 end
 
 % Datos
--- RectaMinimosCuadrados.m
+++ RectaMinimosCuadrados.m
@@ -2,6 +2,7 @@ clc
 clear all
 
 %inicio
+Redondeo = input('ingrese la cantidad de decimales a trabajar :');
 X = input('ingrese los valores de X entre [] y separados por ; :');
 Y = input('ingrese los valores de Y entre [] y separados por ; :');
 [filas,columnas] = size(X);
@@ -10,19 +11,19 @@ XY = filas;
 Error = filas;
 
 % Armo la columna de x al cuadrado
-X2 = X.^2;
+X2 = round(X.^2, Redondeo);
 % Armo la columna de XY 
 for i=1:filas
     Nro(i,1) = i;
-    XY(i,1) = X(i,1) * Y(i,1);
+    XY(i,1) = round(X(i,1) * Y(i,1),Redondeo);
 end
 
 % Sumatorias de las columnas
-sumatoriaN = sum(Nro(:,1));
-sumatoriaX = sum(X(:,1));
-sumatoriaX2 = sum(X2(:,1));
-sumatoriaY = sum(Y(:,1));
-sumatoriaXY = sum(XY(:,1));
+sumatoriaN = round(sum(Nro(:,1)),Redondeo);
+sumatoriaX = round(sum(X(:,1)),Redondeo);
+sumatoriaX2 = round(sum(X2(:,1)),Redondeo);
+sumatoriaY = round(sum(Y(:,1)),Redondeo);
+sumatoriaXY = round(sum(XY(:,1)),Redondeo);
 
 % Sistemas de Ecuaciones
 a = [sumatoriaX2, sumatoriaX; sumatoriaX, filas];
@@ -31,13 +32,13 @@ resultado = a\b;
 disp(resultado);
 
 % Funcion P(x) = ax+b
-recorrido=0:20;
+recorrido=0:10;
 P = resultado(1,1)*recorrido + resultado(2,1);
 
 % Error
 for i=1:filas
-    Px = resultado(1,1)*X(i,1) + resultado(2,1);
-    Error(i,1) = (Px - Y(i,1))^2;
+    Px = round(resultado(1,1)*X(i,1) + resultado(2,1), Redondeo);
+    Error(i,1) = round((Px - Y(i,1))^2, Redondeo);
 end
 
 % Datos
